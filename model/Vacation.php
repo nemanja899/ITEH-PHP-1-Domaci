d94d6cd1703660fad7c4819e19ee23a2174d0b79
nemanja899 <zakizaja@hotmail.com>
2022-08-16 11:25:18 +0200

    Ispravka modela Vacation

---
+++ model/Vacation.php
@@ -0,0 +1,22 @@
+<?php
+class Vacation{
+
+    public $ID;
+    public $Name;
+    public $Description;
+    public $Price;
+    public $Place;
+
+    public function __construct($ID=null,$Name=null,$Description=null,$Price=null,$Place=null)
+    {
+        $this->ID=$ID;
+        $this->Name=$Name;
+        $this->Description=$Description;
+        $this->Price=$Price;
+        $this->Place=$Place;
+    }
+}
+
+
+
+?>
